b9461cd1da995e17c0d05741ee7d15a70bab69bb
Maria-Cristiana Gîrjău <51865225+mcgirjau@users.noreply.github.com>
2020-10-22 20:09:12 -0400

    Update weekend_or_holiday.R

--- R/weekend_or_holiday.R
+++ R/weekend_or_holiday.R
@@ -7,20 +7,11 @@
 #' @examples
 #' weekend_or_holiday("2019-07-01")
 #' 
-#' 
-library(tidyverse)
-library(lubridate)
-
+#' @export
 weekend_or_holiday <- function(d) {
-  
   
   holidays <- lubridate::ymd(c("2018-07-04", "2018-09-03", "2018-10-08", "2018-11-22",
                                "2019-05-25", "2019-07-04", "2019-09-07", "2019-11-26", "2019-10-12"))
   
-  
-  if(wday(d, label = TRUE) == "Sat" || wday(d, label = TRUE) == "Sun" || d %in% holidays ){
-    return(TRUE)
-    
-  } else
-    return(FALSE)
+  return(wday(d, label = TRUE) == "Sat" || wday(d, label = TRUE) == "Sun" || d %in% holidays)
 }
