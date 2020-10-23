fcc21c64fe775b17ab1eb58083f5a5b5ca8c2b76
Maria-Cristiana Gîrjău <51865225+mcgirjau@users.noreply.github.com>
2020-10-22 20:08:01 -0400

    Update num_weekend_trips.R

--- R/num_weekend_trips.R
+++ R/num_weekend_trips.R
@@ -6,24 +6,12 @@
 #' 
 #' @examples
 #' userid("478b8a07-cb2d-419f-8ea6-c4c14132bb92")
+#' @export
+num_weekend_trips <- function(userid) {
 
-library(tidyverse)
-library(valleybikeData)
-library(purrr)
-
-source("~/valleybike/R/weekend_or_holiday.R")
-
-num_weekend_trips <- function(userid){
-  
-  
-  
-  user_trips <- trips %>%
-    filter(user_id == userid) 
-  
-  weekend = purrr::map_int(.x = user_trips$start_time, .f = weekend_or_holiday)
+  user_trips <- filter(trips, user_id == userid)
+    
+  weekend <- purrr::map_int(.x = user_trips$start_time, .f = weekend_or_holiday)
   
   return(sum(weekend))
-  
-  
 }
-
