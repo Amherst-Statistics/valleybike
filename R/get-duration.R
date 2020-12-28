da2861780822d637805eaa98caf38c654066135b
mcgirjau <mgirjau21@amherst.edu>
2020-12-28 18:19:46 -0500

    Finish get_duration

---
+++ R/get-duration.R
@@ -0,0 +1,42 @@
+#' Get trip duration
+#'
+#' Get the duration of a trip, as identified through its route ID.
+#'
+#' @title get_duration
+#'
+#' @param route_id A trip's route ID (as a string).
+#' @param unit The measurement unit for the duration (one of: "seconds", "minutes", or "hours").
+#'             Defaults to "minutes".
+#'
+#' @return The approximate duration of the trip (in the measurement unit specified).
+#'
+#' @examples
+#' \dontrun{
+#' get_duration("route_04_2019@01253ad9-cfb8-44a5-9218-bff8109e4880")
+#' }
+#'
+#' @export
+get_duration <- function(route_id, unit = "minutes") {
+
+  if (!(route_id %in% valleybikeData::trips$route_id)) {
+    stop("Invalid route ID \"", route_id, "\".")
+  }
+
+  if (!(unit %in% c("seconds", "minutes", "hours"))) {
+    stop("Unsupported measurement unit \"", unit,
+         "\". Must be one of: \"seconds\", \"minutes\", \"hours\".")
+  }
+
+  duration_seconds <- valleybikeData::trips %>%
+    dplyr::filter(route_id == !!route_id) %>%
+    dplyr::pull(duration)
+
+  duration <- switch(
+    unit,
+    "seconds" = duration_seconds,
+    "minutes" = duration_seconds / 60,
+    "hours" = duration_seconds / 3600
+  )
+
+  return(duration)
+}
