f767eab12325a5f70473eca930665168d1088ac3
mcgirjau <mgirjau21@amherst.edu>
2020-12-28 18:19:23 -0500

    Finish get_trajectory_data

---
+++ R/get-trajectory-data.R
@@ -0,0 +1,30 @@
+#' Get trajectory data
+#'
+#' Get the point-by-point trajectory data for a trip, as identified through its route ID.
+#'
+#' @title get_trajectory_data
+#'
+#' @param route_id A trip's route ID (as a string).
+#'
+#' @return A data frame of trajectory data for the given route ID.
+#'
+#' @examples
+#' \dontrun{
+#' get_trajectory_data("route_07_2018@44bde968-5454-4db2-a4b1-a32493186f82")
+#' }
+#'
+#' @export
+get_trajectory_data <- function(route_id) {
+
+  if (!(route_id %in% valleybikeData::trips$route_id)) {
+    stop("Invalid route ID \"", route_id, "\".")
+  }
+
+  trip_month <- substr(route_id, start = 7, stop = 8)
+  trip_year <- substr(route_id, start = 10, stop = 13)
+
+  data <- valleybikeData::get_monthly_dataset(month = trip_month, year = trip_year) %>%
+    dplyr::filter(route_id == !!route_id)
+
+  return(data)
+}
